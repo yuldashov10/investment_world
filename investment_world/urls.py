7a4556ab1285157ace99d8390db0bf18383c6ec0
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-08-06 04:35:56 +0300

    Настройки `Docker` и `Docker Compose`

--- investment_world/urls.py
+++ investment_world/urls.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from investment_world.views import index
 
 urlpatterns = [
+    path("", index),
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
+    )
